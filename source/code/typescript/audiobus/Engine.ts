00f9340d603c3fce8d9af0e13bea2891c076150b
designerzen <designerzen@gmail.com>
2017-06-15 17:18:14 +0100

    Had to take out the polyfill in Engine as was borking the compiler

--- source/code/typescript/audiobus/Engine.ts
+++ source/code/typescript/audiobus/Engine.ts
@@ -5,14 +5,19 @@ MIT Licence
 Engine
 ==============
 Abstract    - As no bus is useful without an engine
+
 Description - Essential part of the AudioBus package that talks with hardware
                 also ensures that only ever one engine is created
+
 Use         - Static access
-Methods     - Engine.fetch();
+
+Methods     - Engine.fetch() returns an AudioContext
 
 //////////////////////////////////////////////////////////////////////////////*/
 export default class Engine
 {
+  public static VERSION:string = "3.0.0";
+
   public static context:AudioContext;
 
 	public static fetch():AudioContext
