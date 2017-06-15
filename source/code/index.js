1d353ccf8e4200aa34aa87b0715d8aa69894de56
designerzen <designerzen@gmail.com>
2017-06-15 17:22:24 +0100

    Index js for packaging

--- source/code/index.js
+++ source/code/index.js
@@ -6,12 +6,14 @@ Welcome to audioBUS v3 :D
 */
 
 // firstly import any styles relevent to this component...
-//
-
+// they all get merged into one super quick file so you can add many here
 import Styles from '../assets/styles/style.less';
-// import Index from '../../markup/index.pug';
 
+// any html dependencies (become extracted)
+import Index from '../markup/home.pug';
+
+// any other assets you wish to copy...
 import MIDI from '../assets/midi/chopin-polishdance.mid';
 //import MIDI from './test.midi';
 
-import Index from './typescript/index';
+import Scripts from './typescript/index';
