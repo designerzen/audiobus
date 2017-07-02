de03963fc0000d0a95966e3aa9e3c43eb07f674c
designerzen <designerzen@gmail.com>
2017-07-02 02:40:34 +0100

    GMidi instruments testing

--- source/code/typescript/audiobus/io/midi/MidiGeneralInstuments.ts
+++ source/code/typescript/audiobus/io/midi/MidiGeneralInstuments.ts
@@ -1,5 +1,277 @@
 // [General MIDI Instrument Patch Map](https://www.midi.org/specifications/item/gm-level-1-sound-set)
+// [General MIDI Instrument Patch Map](http://cs.uccs.edu/~cs525/midi/midi.html)
+/*
+General MIDI Level 1 Instrument Families
 
+The General MIDI Level 1 instrument sounds are grouped by families.
+In each family are 8 specific instruments.
+
+PC# 	Family Name
+1-8 	Piano
+9-16 	Chromatic Percussion
+17-24 	Organ
+25-32 	Guitar
+33-40 	Bass
+41-48 	Strings
+49-56 	Ensemble
+57-64 	Brass
+65-72 	Reed
+73-80 	Pipe
+81-88 	Synth Lead
+89-96 	Synth Pad
+97-104 	Synth Effects
+105-112 	Ethnic
+113-120 	Percussive
+121-128 	Sound Effects
+
+
+On MIDI Channel 10, each MIDI Note number ("Key#") corresponds to a different drum sound, as shown below.
+GM-compatible instruments must have the sounds on the keys shown here.
+
+While many current instruments also have additional sounds above or below the range show here,
+and may even have additional "kits" with variations of these sounds, only these sounds are supported
+by General MIDI Level 1 devices.
+
+Key#
+
+
+Drum Sound
+
+35
+
+
+Acoustic Bass Drum
+
+36
+
+
+Bass Drum 1
+
+37
+
+
+Side Stick
+
+38
+
+
+Acoustic Snare
+
+39
+
+
+Hand Clap
+
+40
+
+
+Electric Snare
+
+41
+
+
+Low Floor Tom
+
+42
+
+
+Closed Hi Hat
+
+43
+
+
+High Floor Tom
+
+44
+
+
+Pedal Hi-Hat
+
+45
+
+
+Low Tom
+
+46
+
+
+Open Hi-Hat
+
+47
+
+
+Low-Mid Tom
+
+48
+
+
+Hi-Mid Tom
+
+49
+
+
+Crash Cymbal 1
+
+50
+
+
+High Tom
+
+51
+
+
+Ride Cymbal 1
+
+52
+
+
+Chinese Cymbal
+
+53
+
+
+Ride Bell
+
+54
+
+
+Tambourine
+
+55
+
+
+Splash Cymbal
+
+56
+
+
+Cowbell
+
+57
+
+
+Crash Cymbal 2
+
+58
+
+
+Vibraslap
+
+59
+
+
+Ride Cymbal 2
+
+60
+
+
+Hi Bongo
+
+61
+
+
+Low Bongo
+
+62
+
+
+Mute Hi Conga
+
+63
+
+
+Open Hi Conga
+
+64
+
+
+Low Conga
+
+65
+
+
+High Timbale
+
+66
+
+
+Low Timbale
+
+67
+
+
+High Agogo
+
+68
+
+
+Low Agogo
+
+69
+
+
+Cabasa
+
+70
+
+
+Maracas
+
+71
+
+
+Short Whistle
+
+72
+
+
+Long Whistle
+
+73
+
+
+Short Guiro
+
+74
+
+
+Long Guiro
+
+75
+
+
+Claves
+
+76
+
+
+Hi Wood Block
+
+77
+
+
+Low Wood Block
+
+78
+
+
+Mute Cuica
+
+79
+
+
+Open Cuica
+
+80
+
+
+Mute Triangle
+
+81
+
+
+Open Triangle
+*/
 export const MidiInstrumentCodes = [
 	"acoustic grand piano",
 	"bright acoustic piano",
